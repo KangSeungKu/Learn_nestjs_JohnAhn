c240139e5921fa22cde2c0b85dcaa6c8d70bc8e5
SeungKu_Kang <kangsg26@naver.com>
2023-12-11 02:45:28 +0900

    * 데이터베이스 연결 코드 추가 (진행중)
    - TypeORM 버전으로 인해 삽질을 엄청함...

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,27 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { DataSource, Repository } from 'typeorm';
 import { Board } from './board.entity';
+import { BoardStatus } from './board-status.enum';
+import { InjectRepository } from '@nestjs/typeorm';
 
-@EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+// @EntityRepository(Board)
+// @Injectable()
+export class BoardRepository extends Repository<Board> {
+  constructor(@InjectRepository(Board) private dataSource: DataSource) {
+    // super(Board, dataSource.createEntityManager());
+    super(Board, dataSource.manager);
+  }
+
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    const { title, description } = createBoardDto;
+
+    const board = this.create({
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    });
+
+    await this.save(board);
+    return board;
+  }
+}
